77d8da7d6c21248e90992a9a48b9a5e6b610e1ab
Manh Hung <thpt10dmanhhung@gmail.com>
2021-08-26 13:46:46 +0700

    chapter 8: basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ApplicationRecord
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: true
   has_secure_password
+
+  def self.digest string
+    cost =  if ActiveModel::SecurePassword.min_cost
+              BCrypt::Engine::MIN_COST
+            else
+              BCrypt::Engine.cost
+            end
+    BCrypt::Password.create string, cost: cost
+  end
 end
